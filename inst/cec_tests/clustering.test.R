fc53304af6a9b11b46989ff1fe16da738b95bd3d
azureblue <kk0xf0@gmail.com>
2014-08-20 00:04:16 +0200

    Renaming cec1.d to cec1.dp.

--- inst/cec_tests/clustering.test.R
+++ inst/cec_tests/clustering.test.R
@@ -4,7 +4,7 @@ setup <- function()
 {
   M <- as.matrix(read.table(system.file("cec_tests", "mouse1.data", package="cec")))
   C <- as.matrix(read.table(system.file("cec_tests", "centers1.data", package="cec")))
-  expected <- dget(system.file("cec_tests", "cec1.d", package="cec"))
+  expected <- dget(system.file("cec_tests", "cec1.dp", package="cec"))
   CEC <- cec(M, C)
 }
 test.clustering.mouse1 <- function()
