07b8d04530abf3b8ec3126769b98f5c8b550729a
azureblue <kk0xf0@gmail.com>
2014-10-08 20:06:29 +0200

    changing error message

--- src/errors.c
+++ src/errors.c
@@ -8,4 +8,4 @@ const char * const MALLOC_ERROR_MSG = "Memory allocation error.";
 const char * const UNKNOWN_ERROR_MSG = "Internal library error.";
 
 const char * const ALL_CLUSTERS_REMOVED_MSG =
-	"All clusters has been removed before first iteraions (perhaps too many clusters).";
+	"All clusters have been removed before first iteraions.";
