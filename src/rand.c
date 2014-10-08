05bac0fe6c3b3f48bc52ba4b2cb0f46d192ddaf3
azureblue <kk0xf0@gmail.com>
2014-10-08 20:05:23 +0200

    adding RNG abstraction

---
+++ src/rand.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "rand.h"
+#ifdef R_RAND
+#include<R_ext/Random.h>
+#endif
+void cec_rand_init()
+{
+#ifdef R_RAND
+    GetRNGstate();
+#endif
+}
+void cec_rand_end() 
+{
+#ifdef R_RAND
+    PutRNGstate();
+#endif
+}
+
+double cec_rand()
+{
+#ifdef R_RAND
+    return unif_rand();    
+#else
+    return (double) rand() / (double) RAND_MAX;
+#endif
+}
+
---
+++ src/rand.h
@@ -0,0 +1,10 @@
+#ifndef RANDOM_H
+#define	RANDOM_H
+
+void cec_rand_init();
+void cec_rand_end();
+
+double cec_rand();
+
+#endif	/* RANDOM_H */
+
