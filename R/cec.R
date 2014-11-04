b8033a9f19f6bc43f37b780b9abce951c79cff05
azureblue <kk0xf0@gmail.com>
2014-11-04 23:07:22 +0100

    adding checking for NA values, simplifying obtaining model covariances, reindenting the code

--- R/cec.R
+++ R/cec.R
@@ -11,17 +11,22 @@ cec <- function(
   keep.removed  = F,
   interactive   = F,
   readline      = T
-  )
+)
 { 
   ## checking arguments
-
+  
   if (!hasArg(x)) stop("Missing requierd argument: 'x'.")
   if (!hasArg(centers)) stop("Missing requierd argument: 'centers'.")
   
   if (!is.matrix(x)) stop("Illegal argument: 'x' is not a matrix.")
   if (ncol(x) < 1) stop("Illegal argument: ncol(x) < 1.")
-  if (nrow(x) < 1) stop("Illegal argument: nrow(x) < 1.")  
-    
+  if (nrow(x) < 1) stop("Illegal argument: nrow(x) < 1.")
+  
+  ## check for NAs
+  
+  if (!all(complete.cases(x))) stop("Illegal argument: 'x' contains NA values.")
+  if (!all(complete.cases(centers))) stop("Illegal argument: 'centers' contains NA values.")
+  
   var.centers <- F
   if (!is.matrix(centers))
   {
@@ -38,7 +43,7 @@ cec <- function(
   } 
   else 
   {
-    if (ncol(x) != ncol(centers)) stop("Illegal argument: number of columns of 'x' and 'centers' are not equal.")
+    if (ncol(x) != ncol(centers)) stop("Illegal argument: ncol(x) != ncol(centers).")
     if (nrow(centers) < 1) stop("Illegal argument: nrow(centers) < 1.")
     centers.initilized <- T
   }
@@ -53,140 +58,132 @@ cec <- function(
   
   if (var.centers && length(type) > 1)
     stop("Illegal argument: 'type' with length > 1 is not supported for variable number of centers ('centers' as a vector).")
+  
   ####################################################
   
   # run interactive mode if requested
   
   if (interactive)
     return(cec_interactive(x, centers, type, iter.max, 1, param, centers.init, card.min, keep.removed, readline))
-    
+  
   ####################################################
   
   n = ncol(x)
   m = nrow(x)
   k <- NULL
-  if (is.matrix(centers)) k <- nrow(centers) 
-  else k <- centers
- 
-  if (substr(card.min, nchar(card.min), nchar(card.min)) == "%") 
-  {
-    card.min = as.integer(as.double(substr(card.min , 1, nchar(card.min) - 1)) * m / 100)    
-  } 
-  else
-  {
-    card.min = as.integer(card.min)
-  }  
+  if (is.matrix(centers)) 
+    k <- nrow(centers) 
+  else 
+    k <- centers
   
+  if (substr(card.min, nchar(card.min), nchar(card.min)) == "%") 
+    card.min = as.integer(as.double(substr(card.min , 1, nchar(card.min) - 1)) * m / 100)
+  else
+    card.min = as.integer(card.min)
+  
+  # card.min must be greater than the dimension of data
   card.min = max(card.min, n + 1)
   
-    tenergy = .Machine$integer.max
-    Z <- NULL
-    ok.flag <- F    
-    if (!var.centers)
+  tenergy = .Machine$integer.max
+  Z <- NULL
+  ok.flag <- F    
+  if (!var.centers)
+  {
+    params <- create.cec.params(k, n, type, param)
+    types <- as.integer(vapply(type, resolve.type, 0))
+    if (length(types) == 1) 
+      types <- rep(types, k)
+  }
+  
+  startTime <- proc.time()     
+  
+  for (start in 1:nstart) 
+  {      
+    if (var.centers)
     {
+      k <- centers[start]        
       params <- create.cec.params(k, n, type, param)
       types <- as.integer(vapply(type, resolve.type, 0))
       if (length(types) == 1) 
         types <- rep(types, k)
     }
-    startTime <- proc.time()     
-    for (start in 1:nstart) 
-    {      
-      if (var.centers)
+    if (!centers.initilized) 
+      centers.matrix <- initcenters(x, k, centers.init)     
+    else
+      centers.matrix <- centers      
+    
+    tryCatch(
       {
-        k <- centers[start]        
-        params <- create.cec.params(k, n, type, param)
-        types <- as.integer(vapply(type, resolve.type, 0))
-        if (length(types) == 1) 
-          types <- rep(types, k)
-      }
-      if (!centers.initilized) 
-        centers.matrix <- initcenters(x, k, centers.init)     
-      else
-        centers.matrix <- centers      
-      
-      tryCatch( 
-       {
-         X <- .Call(cec_r, x, centers.matrix, iter.max, types, card.min, params)  
-         ok.flag <- T
-         if (X$iterations < 0 || X$energy[X$iterations + 1] < tenergy)
-         {
-           tenergy = X$energy[X$iterations+1]
-           Z <- X
-           k.final <- k
-           types.final <- types
-           params.final <- params
-         }
-       }, error = function(er) {
-         warning(paste("Error at start #", start, ": ", er$message), immediate.=T, call.=F)
-       }
-     )     
-    }
-     if (ok.flag == F) 
-     {
-       stop("All starts faild with error.")
-     }    
-    execution.time = as.vector((proc.time() - startTime))[1]
-  
-    Z$centers[is.nan(Z$centers)] <- NA
-  
-    tab <- tabulate(Z$cluster)
-    probability <- vapply(tab, function(c.card){c.card / m}, 0)
-  
-    if (!keep.removed)
-    {
-      cluster.map = 1:k.final
-      na.rows = which(is.na(Z$centers[, 1]))
-      if (length(na.rows) > 0)
-      {
-        for (i in 1:length(na.rows))        
-          for (j in na.rows[i]:k.final)          
-            cluster.map[j] <- cluster.map[j] - 1    
-        
-        Z$cluster  <- as.integer(vapply(Z$cluster,function(asgn) {as.integer(cluster.map[asgn])}, 0))
-        
-        #in case of having single row - convert it to a matrix
-        Z$centers <- matrix(Z$centers[-na.rows,],,n)
-        
-        Z$covariances <- Z$covariances[-na.rows]
-        probability <- probability[-na.rows]
-        params.final <- params.final[-na.rows]
-        types.final <- types.final[-na.rows]
-      }     
-    }
-    structure(list(
-      data                = x,
-      cluster             = Z$cluster,
-      centers             = Z$centers, 
-      probability         = probability,
-      cost.function       = Z$energy  [1:(Z$iterations + 1)], 
-      nclusters           = Z$nclusters,
-      final.cost.function = Z$energy  [Z$iterations + 1], 
-      final.nclusters     = Z$nclusters[Z$iterations + 1],
-      iterations          = Z$iterations, 
-      time                = execution.time,
-      covariances         = Z$covariances,
-      covariances.model   = lapply(list.triple(types.final, Z$covariances, params.final), model.covariance)
-    ), class = "cec");    
-}
-
-list.triple <- function(type.v, cov.l, param.l)
-{
-  len <- length(type.v)
-  L <- rep(list(NA), len)
-  for (i in 1:len)
-  {
-    L[[i]] <- list(type.v[i], cov.l[[i]], param.l[[i]])
+        X <- .Call(cec_r, x, centers.matrix, iter.max, types, card.min, params)  
+        ok.flag <- T
+        if (X$iterations < 0 || X$energy[X$iterations + 1] < tenergy)
+        {
+          tenergy = X$energy[X$iterations+1]
+          Z <- X
+          k.final <- k
+          types.final <- types
+          params.final <- params
+        }
+      }, error = function(er) {
+        warning(paste("Error at start #", start, ": ", er$message), immediate.=T, call.=F)
+      })     
   }
-  L
+  
+  if (ok.flag == F) 
+  {
+    stop("All starts faild with error.")
+  }    
+  execution.time = as.vector((proc.time() - startTime))[1]
+  
+  Z$centers[is.nan(Z$centers)] <- NA
+  
+  tab <- tabulate(Z$cluster)
+  probability <- vapply(tab, function(c.card){c.card / m}, 0)
+  
+  if (!keep.removed)
+  {
+    #change cluster assignment
+    cluster.map = 1:k.final
+    na.rows = which(is.na(Z$centers[, 1]))
+    if (length(na.rows) > 0)
+    {
+      for (i in 1:length(na.rows))        
+        for (j in na.rows[i]:k.final)          
+          cluster.map[j] <- cluster.map[j] - 1    
+      
+      Z$cluster  <- as.integer(vapply(Z$cluster,function(asgn) {as.integer(cluster.map[asgn])}, 0))
+      
+      #in case of having single row - convert it to a matrix
+      Z$centers <- matrix(Z$centers[-na.rows,],,n)        
+      Z$covariances <- Z$covariances[-na.rows]
+      probability <- probability[-na.rows]
+      params.final <- params.final[-na.rows]
+      types.final <- types.final[-na.rows]
+    }     
+  }
+  covs = length(types.final)
+  covariances.model = rep(list(NA), covs)
+  for(i in 1:covs)
+    covariances.model[[i]] = model.covariance(types.final[[i]], Z$covariances[[i]], params.final[[i]])
+  
+  structure(list(
+    data                = x,
+    cluster             = Z$cluster,
+    centers             = Z$centers, 
+    probability         = probability,
+    cost.function       = Z$energy[1:(Z$iterations + 1)], 
+    nclusters           = Z$nclusters,
+    final.cost.function = Z$energy[Z$iterations + 1], 
+    final.nclusters     = Z$nclusters[Z$iterations + 1],
+    iterations          = Z$iterations, 
+    time                = execution.time,
+    covariances         = Z$covariances,
+    covariances.model   = covariances.model
+  ), class = "cec");    
 }
 
-model.covariance <- function(type.cov.param)
+model.covariance <- function(type, cov, param)
 {
-  type  <- type.cov.param[[1]]
-  cov   <- type.cov.param[[2]]
-  param <- type.cov.param[[3]]
-  
   if (length(which(is.na(cov))) > 0)
   {
     matrix(NA, nrow(cov), ncol(cov))
@@ -208,11 +205,9 @@ model.covariance <- function(type.cov.param)
     cov * diag(ncol(cov))
   }
   else if (type == resolve.type("eigenvalues"))
-  {
-    
+  {    
     V <- eigen(cov)$vec
-    D <- diag(sort(param, decreasing=T))
-    
+    D <- diag(sort(param, decreasing=T))    
     V %*% D %*% t(V)
   }
   else if (type == resolve.type("all"))
@@ -251,26 +246,26 @@ plot.cec <- function(x, col, cex = 0.5, pch = 16, cex.centers = 1, pch.centers =
   plot(x$data, col=col, cex = cex, pch = pch,  xlab = xlab, ylab = ylab, ...)    
   points(x$centers, cex = cex.centers, pch = pch.centers) 
   if (x$iterations > -1)
-  if (ellipses)
-  {    
-    for (i in 1:nrow(x$centers))     
-      if (! is.na(x$centers[i, 1]))
-      {         
-        err = FALSE        
-        tryCatch(
-          {
-            cov <- NA
-            if (model == T)
-              cov <- x$covariances.model[[i]]
-            else
-              cov <- x$covariances[[i]]
-            pts <- ellipse(x$centers[i, ], cov)
-            lines(pts, lwd = ellipses.lwd)
-          },
-       #   warning = function(e) {warning("some ellipses will not be drawn (probably not positive-definite covariance matrix)")},
-       #   error = function(e) {warning("some ellipses will not be drawn (probably not positive-definite covariance matrix)")}, 
-          finally = {})     
-      }
+    if (ellipses)
+    {    
+      for (i in 1:nrow(x$centers))     
+        if (! is.na(x$centers[i, 1]))
+        {         
+          err = FALSE        
+          tryCatch(
+            {
+              cov <- NA
+              if (model == T)
+                cov <- x$covariances.model[[i]]
+              else
+                cov <- x$covariances[[i]]
+              pts <- ellipse(x$centers[i, ], cov)
+              lines(pts, lwd = ellipses.lwd)
+            },
+            #   warning = function(e) {warning("some ellipses will not be drawn (probably not positive-definite covariance matrix)")},
+            #   error = function(e) {warning("some ellipses will not be drawn (probably not positive-definite covariance matrix)")}, 
+            finally = {})     
+        }
     }  
 }
 
@@ -296,65 +291,65 @@ cec_interactive <- function(
 )
 {
   par 
+{
+  old.ask = par()["ask"]   
+  n = ncol(x)
+  if (n != 2) 
+    stop("interactive mode available only for 2-dimensional data")    
+  i <- -1    
+  if (!is.matrix(centers)) centers <- initcenters(x, centers, centers.init)
+  if (readline)
   {
-    old.ask = par()["ask"]   
-    n = ncol(x)
-    if (n != 2) 
-      stop("interactive mode available only for 2-dimensional data")    
-    i <- -1    
-    if (!is.matrix(centers)) centers <- initcenters(x, centers, centers.init)
-    if (readline)
-    {
-      ignore = readline(prompt="After each iteration you may:\n - press <Enter> for next iteration \n - write number <n> (may be negative one) and press <Enter> for next <n> iterations \n - write 'q' and abort execution.\n Press <Return>.\n")        
-      par(ask = FALSE)
-    }
+    ignore = readline(prompt="After each iteration you may:\n - press <Enter> for next iteration \n - write number <n> (may be negative one) and press <Enter> for next <n> iterations \n - write 'q' and abort execution.\n Press <Return>.\n")        
+    par(ask = FALSE)
+  }
+  else
+  {
+    par(ask = TRUE)
+  }
+  while (TRUE) 
+  {      
+    Z <- cec(x, centers, type, i, 1, param, centers.init , card.min, keep.removed, F);
+    
+    if(i > Z$iterations | i>= iter.max) 
+      break
+    
+    desc = ""
+    if (i == -1)
+      desc = "(initial cluster centers and first assignment to groups)"
+    else if (i == 0)
+      desc = "(position of center means before first iteration)"      
+    
+    cat("Iterations:", Z$iterations, desc, "cost function:", Z$cost[(Z$iterations + 1)]," \n ")
+    
+    if (i == -1)
+      plot(Z, ellipses = FALSE)
     else
-    {
-      par(ask = TRUE)
-    }
-    while (TRUE) 
-    {      
-      Z <- cec(x, centers, type, i, 1, param, centers.init , card.min, keep.removed, F);
-      
-      if(i > Z$iterations | i>= iter.max) 
-        break
-      
-      desc = ""
-      if (i == -1)
-        desc = "(initial cluster centers and first assignment to groups)"
-      else if (i == 0)
-        desc = "(position of center means before first iteration)"      
-      
-      cat("Iterations:", Z$iterations, desc, "cost function:", Z$cost[(Z$iterations + 1)]," \n ")
-      
-      if (i == -1)
-        plot(Z, ellipses = FALSE)
-      else
-        plot(Z, ellipses = TRUE)
-      
-      if (readline) 
-      {
-        line = readline(prompt="Press <Enter> OR write number OR write 'q':");     
-        lineint = suppressWarnings(as.integer(line))
-        
-        if (!is.na(lineint)) 
-        {
-          i = i + lineint - 1
-          if (i < -1) 
-            i = -2        
-        } 
-        else if (line == "q" | line == "quit") {
-          break
-        }
-      }
-      i = i + 1
-    }    
-    plot(Z, ellipses="TRUE")
-    par(ask = old.ask)
+      plot(Z, ellipses = TRUE)
+    
     if (readline) 
     {
-      ignore = readline(prompt="Press <Enter>:")
+      line = readline(prompt="Press <Enter> OR write number OR write 'q':");     
+      lineint = suppressWarnings(as.integer(line))
+      
+      if (!is.na(lineint)) 
+      {
+        i = i + lineint - 1
+        if (i < -1) 
+          i = -2        
+      } 
+      else if (line == "q" | line == "quit") {
+        break
+      }
     }
-    Z
+    i = i + 1
+  }    
+  plot(Z, ellipses="TRUE")
+  par(ask = old.ask)
+  if (readline) 
+  {
+    ignore = readline(prompt="Press <Enter>:")
   }
+  Z
+}
 }
