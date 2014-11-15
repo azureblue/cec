213577aefb227cf6fc217777a76f661ac542d376
azureblue <kk0xf0@gmail.com>
2014-11-15 15:10:41 +0100

    removing braces

--- src/array.c
+++ src/array.c
@@ -3,59 +3,45 @@
 void array_fill(double * x, const double val, const int n)
 {
     for (int i = 0; i < n; i++)
-    {
 	x[i] = val;
-    }
 }
 
 void array_copy(const double * from, double * to, const int n)
 {
     for (int i = 0; i < n; i++)
-    {
 	to[i] = from[i];
-    }
 }
 
 void array_add(double * x1, const double *x2, const int n)
 {
     for (int i = 0; i < n; i++)
-    {
 	x1[i] += x2[i];
-    }
 }
 
 void array_sub(double * x1, const double * x2, const int n)
 {
     for (int i = 0; i < n; i++)
-    {
 	x1[i] -= x2[i];
-    }
 }
 
 void array_sum_multiplied(double * dest, const double * x1, const double m1,
 	const double *x2, const double m2, const int n)
 {
     for (int i = 0; i < n; i++)
-    {
 	dest[i] = x1[i] * m1 + x2[i] * m2;
-    }
 }
 
 void array_mul(double * x1, const double val, const int n)
 {
     for (int i = 0; i < n; i++)
-    {
 	x1[i] *= val;
-    }
 }
 
 double dist2(const double * x1, const double * x2, const int n)
 {
     double res = 0;
     for (int i = 0; i < n; i++)
-    {
 	res += (x1[i] - x2[i]) * (x1[i] - x2[i]);
-    }
     return res;
 }
 
