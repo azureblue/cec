19c1fdc1eabf11bd2771a321bbdafd32487abfe3
azureblue <kk0xf0@gmail.com>
2015-01-31 13:03:04 +0100

    adding more checks

--- inst/cec_tests/clustering.test.R
+++ inst/cec_tests/clustering.test.R
@@ -11,5 +11,12 @@ test.clustering.mouse1 <- function()
   CEC <- cec(M, C)
   CEC:::checkNumericVectorEquals(expected$cluster, CEC$cluster, msg="Clustering vector")
   CEC:::checkNumericVectorEquals(expected$cost, CEC$cost, msg="Energy")
-  CEC:::checkNumericMatrixEquals(expected$centers, CEC$centers, msg="Centers")  
+  CEC:::checkNumericMatrixEquals(expected$centers, CEC$centers, msg="Centers")
+  CEC:::checkNumericMatrixEquals(M, CEC$data, msg="Data")
+  CEC:::checkNumericVectorEquals(expected$probability, CEC$probability, msg="Probability")
+  CEC:::checkNumericVectorEquals(expected$nclusters, CEC$nclusters, msg="Number of clusters")
+  CEC:::checkNumericVectorEquals(expected$final.cost.function, CEC$final.cost.function, msg="Final cost function")
+  CEC:::checkNumericVectorEquals(expected$final.nclusters, CEC$final.nclusters, msg="Final number of clusters")
+  CEC:::checkNumericVectorEquals(expected$iterations, CEC$iterations, msg="Iterations")
+  
 }
