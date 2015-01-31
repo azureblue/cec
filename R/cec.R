4a7b68e4830dc80223d97e3155a2a5988a80297e
azureblue <kk0xf0@gmail.com>
2015-01-31 12:09:08 +0100

    moving model.covariance and cec.plot.function to separate files

--- R/cec.R
+++ R/cec.R
@@ -186,40 +186,6 @@ cec <- function(
   ), class = "cec");    
 }
 
-model.covariance <- function(type, cov, param)
-{
-  if (length(which(is.na(cov))) > 0)
-  {
-    matrix(NA, nrow(cov), ncol(cov))
-  }  
-  else if (type == resolve.type("covariance"))
-  {
-    param[[1]]
-  }  
-  else if (type == resolve.type("fixedr"))
-  {
-    diag(ncol(cov)) * param
-  }
-  else if (type == resolve.type("spherical"))
-  {
-    diag(ncol(cov)) * sum(diag(ncol(cov)) * cov) / ncol(cov)
-  }
-  else if (type == resolve.type("diagonal"))
-  {
-    cov * diag(ncol(cov))
-  }
-  else if (type == resolve.type("eigenvalues"))
-  {    
-    V <- eigen(cov)$vec
-    D <- diag(sort(param, decreasing=T))    
-    V %*% D %*% t(V)
-  }
-  else if (type == resolve.type("all"))
-  {
-    cov
-  }
-}
-
 print.cec <- function(x, ...)
 {
   cat("CEC clustering result: \n\n")
@@ -271,13 +237,6 @@ plot.cec <- function(x, col, cex = 0.5, pch = 16, cex.centers = 1, pch.centers =
             finally = {})     
         }
     }  
-}
-
-cec.plot.cost.function <- function(C, xlab="Iteration", ylab="Cost function", lwd=5, col="red", lwd.points=5, pch.points=19, col.points="black", mgp=c(1.5,0.5,0), ...)
-{
-  plot(x = 1:(length(C$cost) - 1), y = C$cost[2:(length(C$cost))], xlab=xlab, ylab=ylab, type="l", lwd=lwd, col=col, mgp=mgp, ...)
-  points(x = 1:(length(C$cost) - 1), y = C$cost[2:(length(C$cost))], lwd=lwd.points, pch=pch.points, col=col.points)
-  title("Cost function at each iteraion")
 }
 
 cec_interactive <- function(
