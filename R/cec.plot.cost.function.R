ff3815b975f7fab526d6bcad2dfaf627b7df5847
azureblue <kk0xf0@gmail.com>
2016-01-17 00:19:58 +0100

    fixing misspelled word in cec.plot.cost.function.R

--- R/cec.plot.cost.function.R
+++ R/cec.plot.cost.function.R
@@ -2,5 +2,5 @@ cec.plot.cost.function <- function(C, xlab="Iteration", ylab="Cost function", lw
 {
     plot(x = 1:(length(C$cost) - 1), y = C$cost[2:(length(C$cost))], xlab=xlab, ylab=ylab, type="l", lwd=lwd, col=col, mgp=mgp, ...)
     points(x = 1:(length(C$cost) - 1), y = C$cost[2:(length(C$cost))], lwd=lwd.points, pch=pch.points, col=col.points)
-    title("Cost function at each iteraion")
+    title("Cost function at each iteration")
 }
