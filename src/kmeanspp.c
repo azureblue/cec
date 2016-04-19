1702a71af7766e1453a04a5e61d867fe6b68a9d7
azureblue <kk0xf0@gmail.com>
2016-04-19 23:16:19 +0200

    kmeanspp: fixing dist calculation and adding randomization when k > m

--- src/kmeanspp.c
+++ src/kmeanspp.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 #include "alloc.h"
 #include "errors.h"
 #include "kmeanspp.h"
@@ -10,14 +8,13 @@ static int binary_search_d(double key, double * array, int len)
     int a = 0, b = len - 1;
     while (a != b)
     {
+        if (array[a] == 0 && array[b] == 0)
+            return (int) (cec_rand() * (b - a + 1)) + a;
         int mid = (a + b) / 2;
         if (array[mid] < key)
-        {
-            a = mid + 1;
-        } else
-        {
+            a = mid + 1;        
+        else
             b = mid;
-        }
     }
     return a;
 }
@@ -27,10 +24,10 @@ int kmeanspp(struct cec_matrix * X, struct cec_matrix * C)
     int m = X->m;
     int k = C->m;
     int n = X->n;
-
+    
     double * dists_m = m_alloc(sizeof (double) * (m + 1));
     double * sums_m = m_alloc(sizeof (double) * (m + 1));
-
+    
     if (dists_m == NULL || sums_m == NULL)
     {
         m_free(dists_m);
@@ -48,35 +45,33 @@ int kmeanspp(struct cec_matrix * X, struct cec_matrix * C)
     
     double * dists = dists_m + 1;
     double * sums = sums_m + 1;
-
+    
     for (int i = 0; i < m; i++)
     {
         double dist = dist2(cec_matrix_row(X, i), cec_matrix_row(C, 0), n);
         dists[i] = dist;
         sums[i] = sums[i - 1] + dist;
     }
-
-
+    
     for (int i = 1; i < k; i++)
     {
         double r = cec_rand();
         double n_sum = r * sums[m - 1];
         int idx = binary_search_d(n_sum, sums, m);
         array_copy(cec_matrix_row(X, idx), cec_matrix_row(C, i), n);
-
-        for (int j = 1; j < m; j++)
+        
+        for (int j = 0; j < m; j++)
         {
             double dist = dist2(cec_matrix_row(X, j), cec_matrix_row(C, i), n);
             if (dist < dists[j])
-            {
                 dists[j] = dist;
-            }
-            sums[j] = sums[j - 1] + dists[j];
+            
+            sums_m[j + 1] = sums_m[j] + dists[j];
         }
     }
-
+    
     cec_rand_end();
-
+    
     m_free(dists_m);
     m_free(sums_m);
     return 0;
