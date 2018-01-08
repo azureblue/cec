2774c95b2417c889413970193e562db7776ba551
azureblue <kk0xf0@gmail.com>
2018-01-08 21:03:14 +0100

    fixing type in R to int

--- R/init.centers.R
+++ R/init.centers.R
@@ -5,6 +5,6 @@ init.centers <- function(x, k, method = c("kmeans++", "random"))
     if (! is.matrix(x)) stop("init.centers: x is not a matrix")
     if (k < 0) stop("init.centers: k < 0")
 
-    centers <- .Call(cec_init_centers_r, x, k, method);
+    centers <- .Call(cec_init_centers_r, x, as.integer(k), method);
     centers  
 }
