9ec6b8c3e30ff72e1d272f5e00ea53133eef6b0d
azureblue <kk0xf0@gmail.com>
2018-01-17 18:50:26 +0100

    fixing routines registration, refactoring points split

--- src/split_starter.h
+++ src/split_starter.h
@@ -1,10 +1,21 @@
 #ifndef CEC_SPLIT_STARTER_H
 #define CEC_SPLIT_STARTER_H
 
+#include "starter.h"
 
-class split_starter {
+namespace cec {
+    struct split_starter_params {
+        starter_params params;
+        int starts;
+        int split_tries;
+        int max_k;
+        int max_depth;
+        int threads;
+    };
 
-};
+    class split_starter {
 
+    };
+}
 
 #endif //CEC_SPLIT_STARTER_H
