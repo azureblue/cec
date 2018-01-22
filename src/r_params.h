ea930d13c5fe65b8510b4b75eb7c3795c458cfc0
azureblue <kk0xf0@gmail.com>
2018-01-22 13:56:53 +0100

    refactoring r params

--- src/r_params.h
+++ src/r_params.h
@@ -2,16 +2,19 @@
 #define CEC_PARAMS_R_H
 
 #include "params.h"
+#include "r_ext_ptr.h"
 #include <Rdefines.h>
 
 namespace cec {
-    centers_param get_centers_param(SEXP centers_param_r);
+    namespace r {
+        r_ext_ptr<centers_param> get_centers_param(SEXP centers_param_r);
 
-    control_param get_control_param(SEXP control_param_r);
+        r_ext_ptr<control_param> get_control_param(SEXP control_param_r);
 
-    models_param get_models_param(SEXP models_param_r, int n);
+        r_ext_ptr<models_param> get_models_param(SEXP models_param_r, int n);
 
-    split_param get_split_param(SEXP split_param_r);
+        r_ext_ptr<split_param> get_split_param(SEXP split_param_r);
+    }
 }
 
 #endif //CEC_PARAMS_R_H
