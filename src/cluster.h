139e282bbab4761da974c7b652d63f1661d3acb0
azureblue <kk0xf0@gmail.com>
2018-01-27 21:30:27 +0100

    refactoring cluster.h

--- src/cluster.h
+++ src/cluster.h
@@ -6,7 +6,7 @@
 
 namespace cec {
 
-    class mean: public vec {
+    class mean : public vec {
     public:
         explicit mean(const mat &sample)
                 : mean(sample.n) {
@@ -16,8 +16,8 @@ namespace cec {
 
         explicit mean(int n)
                 : vec(n),
-                  acc_(n) {
-            acc_.fill(0);
+                  acc(n) {
+            acc.fill(0);
         }
 
         mean(const mean &initial) = default;
@@ -27,146 +27,198 @@ namespace cec {
         mean &operator=(const mean &m) = default;
 
         void add_point(const row &point) {
-            acc_ += point;
-            card_++;
+            acc += point;
+            car++;
         }
 
         void rem_point(const row &point) {
-            acc_ -= point;
-            card_--;
+            acc -= point;
+            car--;
         }
 
         void update() {
-            set(acc_);
-            (*this) /= card_;
+            row::operator=(acc);
+            (*this) /= car;
         }
 
         int card() const {
-            return card_;
+            return car;
         }
 
     private:
-        int card_ = 0;
-        vec acc_;
+        using row::operator=;
+        int car = 0;
+        vec acc;
     };
 
-    class cluster_utils {
+    class covariance_mat : public mat {
     public:
-        static void cec_cov_add_point(mat &dst_cov, const mat &cov,
-                                      const mean &mean, const row &point) {
-            int card = mean.card();
-            double card_n = card + 1;
-            cec_cov_change(dst_cov, cov, mean, point, card / card_n, card / (card_n * card_n));
+        covariance_mat &operator=(const covariance_mat &cov) = default;
+
+        covariance_mat(const covariance_mat &) = default;
+
+        covariance_mat(covariance_mat &&) noexcept = default;
+
+        const cec::mean &mean() const {
+            return mn;
         }
 
-        static void cec_cov_remove_point(mat &dst_cov, const mat &cov,
-                                         const mean &mean, const row &point) {
-            int card = mean.card();
-            double card_n = card - 1;
-            cec_cov_change(dst_cov, cov, mean, point, card / card_n, -card / (card_n * card_n));
+        int card() const {
+            return mn.card();
         }
 
-    private:
-        static inline void cec_cov_change(mat &dst_cov, const mat &cov,
-                                          const row &mean, const row &point, double cov_mul,
-                                          double new_cov_point_mul) {
-            const int n = cov.n;
-
-            for (int j = 0; j < n; j++)
-                for (int k = 0; k < n; k++)
-                    dst_cov[j][k] = cov[j][k] * cov_mul
-                                    + (mean[j] - point[j]) * (mean[k] - point[k]) * new_cov_point_mul;
-
-        }
-    };
-
-    class covariance_mle {
-    public:
-        static mat estimate(const mat &sample, const mean &mean) {
+        static covariance_mat estimate(const mat &sample) {
+            cec::mean mn(sample);
             int n = sample.n;
             mat acc(n, n);
             acc.fill(0);
-            vec t_vec(acc.n);
+            vec t_vec(n);
             for (auto &&p : sample) {
                 t_vec = p;
-                t_vec -= mean;
+                t_vec -= mn;
                 acc += mat::outer_product(t_vec);
             }
             acc /= sample.m;
-            return acc;
+            return covariance_mat(acc, mn);
+        }
+
+        void add_point(const row &point) {
+            int card = mn.card();
+            double card_n = card + 1;
+            cov_change(point, card / card_n, card / (card_n * card_n), *this);
+            mn.add_point(point);
+            mn.update();
+        }
+
+        void rem_point(const row &point) {
+            int card = mn.card();
+            double card_n = card - 1;
+            cov_change(point, card / card_n, -card / (card_n * card_n), *this);
+            mn.rem_point(point);
+            mn.update();
+        }
+
+    protected:
+        cec::mean mn;
+
+        covariance_mat(mat initial, cec::mean mn)
+                : mat(std::move(initial)),
+                  mn(std::move(mn)) {}
+
+        inline void cov_change(const row &point, double cov_mul,
+                               double new_cov_point_mul, mat &dst) {
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    dst[j][k] = (*this)[j][k] * cov_mul
+                                + (mn[j] - point[j])
+                                  * (mn[k] - point[k])
+                                  * new_cov_point_mul;
         }
     };
 
-    class cluster {
-
+    class deferred_update_covariance : public covariance_mat {
     public:
-        cluster(const model &mod, mean initial_mean, mat initial_covariance, const int m)
-                :
-                m_(m),
-                model_(mod),
-                mean_(std::move(initial_mean)),
-                cov_(std::move(initial_covariance)),
-                t_cov_(cov_),
-                t_point_(initial_mean.size),
-                energy_(mod.energy(cov_, mean_.card(), m)) {}
+        using mat::operator=;
 
-        double add_point(const row &point) {
-            t_point_ = point;
-            card_change_ = 1;
-            cluster_utils::cec_cov_add_point(t_cov_, cov_, mean_, point);
-            t_energy_ = t_energy();
-            return t_energy_ - energy_;
+        explicit deferred_update_covariance(const covariance_mat &initial)
+                : covariance_mat(initial),
+                  tmp_point(initial.m),
+                  tmp_cov(initial) {}
+
+        void add_point_tmp(const row &point) {
+            tmp_point = point;
+            card_change = 1;
+            int card = covariance_mat::card();
+            double card_n = card + 1;
+            cov_change(point, card / card_n, card / (card_n * card_n), tmp_cov);
         }
 
-        double rem_point(const row &point) {
-            t_point_ = point;
-            card_change_ = -1;
-            cluster_utils::cec_cov_remove_point(t_cov_, cov_, mean_, point);
-            t_energy_ = t_energy();
-            return t_energy_ - energy_;
-
-        }
-
-        const row &mean() const {
-            return mean_;
-        }
-
-        int card() const {
-            return mean_.card();
-        }
-
-        const mat &covariance() const {
-            return cov_;
+        void rem_point_tmp(const row &point) {
+            tmp_point = point;
+            card_change = -1;
+            int card = covariance_mat::card();
+            double card_n = card - 1;
+            cov_change(point, card / card_n, -card / (card_n * card_n), tmp_cov);
         }
 
         void apply_change() {
-            cov_ = t_cov_;
-            if (card_change_ == 1)
-                mean_.add_point(t_point_);
-            else
-                mean_.rem_point(t_point_);
-            mean_.update();
-            energy_ = t_energy_;
+            (*this) = tmp_cov;
+            if (card_change == 1) {
+                mn.add_point(tmp_point);
+                mn.update();
+            } else if (card_change == -1){
+                mn.rem_point(tmp_point);
+                mn.update();
+            }
+            card_change = 0;
         }
 
-        double energy() {
-            return model_.energy(cov_, mean_.card(), m_);
+        int tmp_card() const {
+            return card() + card_change;
+        }
+
+        const mat &tmp_covariance() const {
+            return tmp_cov;
         }
 
     private:
-        double t_energy() {
-            return model_.energy(t_cov_, mean_.card() + card_change_, m_);
+        int card_change = 0;
+        vec tmp_point;
+        mat tmp_cov;
+    };
+
+    class cluster {
+    public:
+        cluster(const model &mod, const covariance_mat &initial_covariance, const int m)
+                : m(m),
+                  mod(mod),
+                  cov(initial_covariance),
+                  eng(mod.energy(cov, cov.card(), m)),
+                  tmp_eng(eng) {}
+
+        double add_point(const row &point) {
+            cov.add_point_tmp(point);
+            tmp_eng = tmp_energy();
+            return tmp_eng - eng;
         }
 
-        const int m_;
-        const model &model_;
-        cec::mean mean_;
-        mat cov_;
-        mat t_cov_;
-        vec t_point_;
-        double energy_;
-        double t_energy_;
-        int card_change_ = 0;
+        double rem_point(const row &point) {
+            cov.rem_point_tmp(point);
+            tmp_eng = tmp_energy();
+            return tmp_eng - eng;
+        }
+
+        const row &mean() const {
+            return cov.mean();
+        }
+
+        int card() const {
+            return cov.card();
+        }
+
+        const mat &covariance() const {
+            return cov;
+        }
+
+        void apply_change() {
+            cov.apply_change();
+            eng = tmp_eng;
+        }
+
+        double energy() {
+            return mod.energy(cov, cov.card(), m);
+        }
+
+    private:
+        double tmp_energy() {
+            return mod.energy(cov.tmp_covariance(), cov.tmp_card(), m);
+        }
+
+        const int m;
+        const model &mod;
+        deferred_update_covariance cov;
+        double eng;
+        double tmp_eng;
     };
 }
 
